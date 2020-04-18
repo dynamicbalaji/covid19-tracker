365718fe904efaa7e91796edbb5457c4a5eee0cb
dynamicbalaji <dynamicbalaji4u@gmail.com>
2020-04-18 15:47:26 +0530

    Modify functional App to stateful App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      Om Muruga
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        Om Muruga!!
     </div>
-  );
+    );
+  }
 }
 
 export default App;
