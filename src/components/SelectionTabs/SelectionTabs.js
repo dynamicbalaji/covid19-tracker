276eaa5aa7435fcab14f8d447ad205825bb3a714
dynamicbalaji <dynamicbalaji4u@gmail.com>
2020-04-18 16:39:00 +0530

    Implement swipeable views for tabs

--- src/components/SelectionTabs/SelectionTabs.js
+++ src/components/SelectionTabs/SelectionTabs.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Paper, Tabs, Tab } from '@material-ui/core';
+import { Paper, Tabs, Tab, useTheme } from '@material-ui/core';
+import SwipeableViews from 'react-swipeable-views';
 
 import './SelectionTabs.css';
 
 const SelectionTabs = () => {
     const [value, setValue] = useState(0);
+    const theme = useTheme();
 
     const handleChange = (event, newVal) => setValue(newVal);
 
@@ -23,6 +25,21 @@ const SelectionTabs = () => {
                     <Tab label="World" />
                 </Tabs>
             </Paper>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChange}
+            >
+                <span value={value} index={0} dir={theme.direction}>
+                    Item One
+                </span>
+                <span value={value} index={1} dir={theme.direction}>
+                    Item Two
+                </span>
+                <span value={value} index={2} dir={theme.direction}>
+                    Item Three
+                </span>
+            </SwipeableViews>
         </div>
     );
 }
