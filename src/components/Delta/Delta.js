3ebe791b835995d92f84fd02c16136cba75c8562
dynamicbalaji <dynamicbalaji4u@gmail.com>
2020-04-25 23:47:58 +0530

    Add count up for delta data

--- src/components/Delta/Delta.js
+++ src/components/Delta/Delta.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import EjectRoundedIcon from '@material-ui/icons/EjectRounded';
+import CountUp from 'react-countup';
 
 import './Delta.css';
 
@@ -7,7 +8,8 @@ const Delta = ({ inpCnt, color, size }) => {
     return (
         <>
             <span className={`delta-data-${color} delta-data-${size}`}>
-                <EjectRoundedIcon fontSize="small" className={`delta-data-${size} delta-data-icon-${size}`} />{inpCnt}{' '}
+                <EjectRoundedIcon fontSize="small" className={`delta-data-${size} delta-data-icon-${size}`} />
+                <CountUp start={0} end={inpCnt ? inpCnt : 0} duration={2.5} separator="," />{' '}
             </span>
         </>
     );
